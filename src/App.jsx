eb9a249141fb76de8d34b56f0ad5faa19bc15005
Jabir-khan12 <jabir.bscs4168@iiu.edu.pk>
2025-09-08 21:50:01 +0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,19 @@ import AdminDashboard from "./components/admin/AdminDashboard";
 import PatientDashboard from "./components/patient/PatientDashboard";
 import PatientList from "./components/admin/PatientManagement";
 import RecommendationForm from "./components/admin/RecommendationForm";
-import ProtectedRoute from "./router/ProtectedRoute"; // ✅ Import
+import ProtectedRoute from "./router/ProtectedRoute";
 
 const App = () => {
   return (
     <Routes>
-      {/* Redirect root (/) to /login */}
       <Route path="/" element={<Navigate to="/login" />} />
-
-      {/* Public routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-      {/* Admin protected routes */}
       <Route element={<ProtectedRoute roles={["admin"]} />}>
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/patients" element={<PatientList />} />
         <Route path="/admin/recommendations" element={<RecommendationForm />} />
       </Route>
-
-      {/* Patient protected routes */}
       <Route element={<ProtectedRoute roles={["patient"]} />}>
         <Route path="/patient/dashboard" element={<PatientDashboard />} />
       </Route>
