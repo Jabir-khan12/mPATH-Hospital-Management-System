a4cc7ac67551855b26bec502584c64b2f91815a8
Jabir-khan12 <jabir.bscs4168@iiu.edu.pk>
2025-09-08 21:51:57 +0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Ensure you have an index.css for global styles
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {" "}
-      {/* ✅ Only one BrowserRouter here */}
       <AuthProvider>
         <App />
       </AuthProvider>
