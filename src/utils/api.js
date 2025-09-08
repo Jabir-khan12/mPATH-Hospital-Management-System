bf27fe5ef62b241ad85ea38c74d445fa29575ed1
Jabir-khan12 <jabir.bscs4168@iiu.edu.pk>
2025-09-08 21:47:03 +0500

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000",
-// });
-
-// export default api;
-
-// src/utils/api.js
 import axios from "axios";
 
 export default axios.create({
@@ -15,11 +6,3 @@ export default axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// fetch(`http://localhost:5000/users/${patientId}`, {
-//   method: "PUT",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(updatedData),
-// });
